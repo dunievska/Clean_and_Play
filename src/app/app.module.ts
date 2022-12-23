62b3203fb2078ecc968d3e5067386584c25ee61f
Emilia Duniewska <kamienie@gmail.com>
2022-12-23 15:47:25 +0100

    Solve CORS issues with proxy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,7 @@ import { TaskEditComponent } from './todo/task-list/task-edit/task-edit.componen
     TaskAddComponent,
     TaskEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
