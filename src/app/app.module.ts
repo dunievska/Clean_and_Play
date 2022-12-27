32c6be025e57caf322ab75ad7dc905d3cbc9539d
Emilia Duniewska <kamienie@gmail.com>
2022-12-27 15:09:37 +0100

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { TaskListComponent } from './todo/task-list/task-list.component';
 import { TaskAddComponent } from './todo/task-add/task-add.component';
-import { FormsModule } from '@angular/forms';
 import { TaskEditComponent } from './todo/task-list/task-edit/task-edit.component';
 
 @NgModule({
@@ -18,7 +19,13 @@ import { TaskEditComponent } from './todo/task-list/task-edit/task-edit.componen
     TaskAddComponent,
     TaskEditComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
