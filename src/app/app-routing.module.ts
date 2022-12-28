c72afa7d5fa1f0d85c7861c12412d474c079851d
Emilia Duniewska <kamienie@gmail.com>
2022-12-28 16:08:26 +0100

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { ParentComponent } from './parent/parent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'parent', component: ParentComponent },
+  { path: 'child', component: ChildComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
