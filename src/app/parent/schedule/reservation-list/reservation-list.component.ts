0a1b3e088a97c036a224f0c2ec00e8cdd64512a2
Emilia Duniewska <kamienie@gmail.com>
2023-01-02 15:21:04 +0100

    Get and display list of reservations

--- src/app/parent/schedule/reservation-list/reservation-list.component.ts
+++ src/app/parent/schedule/reservation-list/reservation-list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from 'src/app/models/reservation';
+import { ScheduleService } from 'src/app/services/schedule.service';
 
 @Component({
   selector: 'app-reservation-list',
   templateUrl: './reservation-list.component.html',
-  styleUrls: ['./reservation-list.component.scss']
+  styleUrls: ['./reservation-list.component.scss'],
 })
-export class ReservationListComponent {
+export class ReservationListComponent implements OnInit {
+  public reservations: Reservation[] = [];
 
+  constructor(private scheduleService: ScheduleService) {}
+
+  ngOnInit(): void {
+    this.scheduleService.getAllReservations().subscribe((loadedRes) => {
+      this.reservations = this.sortReservationsByDate(loadedRes);
+    });
+  }
+
+  public getHowLong(reservation: Reservation) {
+    const time =
+      new Date(reservation.end).getTime() -
+      new Date(reservation.start).getTime();
+    return Math.floor(time / 1000 / 60);
+  }
+
+  private sortReservationsByDate(res: Reservation[]) {
+    return res.sort(
+      (a, b) => new Date(a.start).getTime() - new Date(b.start).getTime()
+    );
+  }
 }
--- src/app/services/schedule.service.ts
+++ src/app/services/schedule.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Reservation } from '../models/reservation';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,10 @@ export class ScheduleService {
   private url = 'api/reservations';
 
   constructor(private http: HttpClient) {}
+
+  public getAllReservations(): Observable<Reservation[]> {
+    return this.http.get<Reservation[]>(this.url);
+  }
 
   public addReservation(reservation: Reservation) {
     return this.http.post<Reservation>(this.url, reservation);
