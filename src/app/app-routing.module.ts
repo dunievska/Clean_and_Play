46fe88c6c6b350e4b7fa2eb89981f1e127465f03
Emilia Duniewska <kamienie@gmail.com>
2023-01-03 19:34:19 +0100

    Add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildTodoComponent } from './child/child-todo/child-todo.component';
 import { ChildComponent } from './child/child.component';
 import { HomeComponent } from './home/home.component';
 import { ParentComponent } from './parent/parent.component';
+import { ScheduleComponent } from './parent/schedule/schedule.component';
+import { TodoComponent } from './parent/todo/todo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'parent', component: ParentComponent },
-  { path: 'child', component: ChildComponent },
+  {
+    path: 'parent',
+    component: ParentComponent,
+    children: [
+      {
+        path: 'todo',
+        component: TodoComponent,
+      },
+      { path: 'reservations', component: ScheduleComponent },
+    ],
+  },
+  {
+    path: 'child',
+    component: ChildComponent,
+    children: [
+      {
+        path: 'todo',
+        component: ChildTodoComponent,
+      },
+    ],
+  },
   { path: '**', redirectTo: '/home' },
 ];
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  public onTasks() {
+    this.router.navigate(['todo'], { relativeTo: this.route });
+  }
+  public onReservations() {
+    this.router.navigate(['reservations'], { relativeTo: this.route });
+  }
 }
