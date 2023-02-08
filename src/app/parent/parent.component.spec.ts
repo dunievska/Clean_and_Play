15519587e903662eb8b85d6bee1205bca318b1f2
Emilia Duniewska <kamienie@gmail.com>
2023-02-08 12:19:44 +0100

    Stubbing unneeded components

--- src/app/parent/parent.component.spec.ts
+++ src/app/parent/parent.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { ParentComponent } from './parent.component';
+import { Component } from '@angular/core';
+
+@Component({ selector: 'app-header', template: '' })
+class HeaderStubComponent {}
+
+@Component({ selector: 'router-outlet', template: '' })
+class RouterOutletStubComponent {}
 
 describe('ParentComponent', () => {
   let component: ParentComponent;
@@ -8,9 +15,13 @@ describe('ParentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ParentComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ParentComponent,
+        HeaderStubComponent,
+        RouterOutletStubComponent,
+      ],
+      imports: [RouterTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ParentComponent);
     component = fixture.componentInstance;
