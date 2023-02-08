1499d5830379f5bb088201daa9b71ea0d5a9c65e
Emilia Duniewska <kamienie@gmail.com>
2023-02-08 13:24:35 +0100

    Stubbing unneeded components

--- src/app/parent/todo/todo.component.spec.ts
+++ src/app/parent/todo/todo.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { TodoComponent } from './todo.component';
+
+@Component({ selector: 'app-task-add', template: '' })
+class TaskAddStubComponent {}
+
+@Component({ selector: 'app-task-list', template: '' })
+class TaskListStubComponent {}
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
@@ -8,9 +14,12 @@ describe('TodoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        TodoComponent,
+        TaskAddStubComponent,
+        TaskListStubComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
