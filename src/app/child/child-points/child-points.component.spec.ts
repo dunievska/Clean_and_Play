e62e726caa1ecad664303cf068b0a85474edb7f8
Emilia Duniewska <kamienie@gmail.com>
2023-02-14 12:03:11 +0100

    Add imports, providers and userService

--- src/app/child/child-points/child-points.component.spec.ts
+++ src/app/child/child-points/child-points.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card';
+import { UserService } from 'src/app/services/user.service';
 import { ChildPointsComponent } from './child-points.component';
 
 describe('ChildPointsComponent', () => {
   let component: ChildPointsComponent;
   let fixture: ComponentFixture<ChildPointsComponent>;
+  let userService: UserService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChildPointsComponent ]
-    })
-    .compileComponents();
+      declarations: [ChildPointsComponent],
+      imports: [MatCardModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [UserService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChildPointsComponent);
     component = fixture.componentInstance;
+    userService = fixture.debugElement.injector.get(UserService);
     fixture.detectChanges();
   });
 
