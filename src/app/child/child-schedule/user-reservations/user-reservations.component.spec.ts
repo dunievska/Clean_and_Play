5ec4c63659f6175d5f2b089d3aa18707088c970e
Emilia Duniewska <kamienie@gmail.com>
2023-02-14 12:24:43 +0100

    Add imports and providers

--- src/app/child/child-schedule/user-reservations/user-reservations.component.spec.ts
+++ src/app/child/child-schedule/user-reservations/user-reservations.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserReservationsComponent } from './user-reservations.component';
+import { ScheduleService } from 'src/app/services/schedule.service';
+import { UserService } from 'src/app/services/user.service';
+import { ErrorService } from 'src/app/services/error.service';
 
 describe('UserReservationsComponent', () => {
   let component: UserReservationsComponent;
@@ -8,9 +12,10 @@ describe('UserReservationsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserReservationsComponent ]
-    })
-    .compileComponents();
+      declarations: [UserReservationsComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [ScheduleService, UserService, ErrorService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(UserReservationsComponent);
     component = fixture.componentInstance;
