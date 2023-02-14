325cba07f8102b2387ed2b61c94535542c200d52
Emilia Duniewska <kamienie@gmail.com>
2023-02-14 12:17:52 +0100

    Stubbing unneeded components

--- src/app/child/child-schedule/child-schedule.component.spec.ts
+++ src/app/child/child-schedule/child-schedule.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { ChildScheduleComponent } from './child-schedule.component';
+
+@Component({ selector: 'app-all-reservations', template: '' })
+class AllReservationsStubComponent {}
+
+@Component({ selector: 'app-user-reservations', template: '' })
+class UserReservationsStubComponent {}
 
 describe('ChildScheduleComponent', () => {
   let component: ChildScheduleComponent;
@@ -8,9 +14,12 @@ describe('ChildScheduleComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChildScheduleComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ChildScheduleComponent,
+        AllReservationsStubComponent,
+        UserReservationsStubComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChildScheduleComponent);
     component = fixture.componentInstance;
